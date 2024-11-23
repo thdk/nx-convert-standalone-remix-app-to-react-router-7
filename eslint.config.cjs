a9cae064f15afd8bbcf157c550430815790ca9d0
Thomas Dekiere <t.dekiere@gmail.com>
2024-11-23 13:30:23 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/vF4eCHmDkw

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    ignores: ['build', 'public/build'],
+  },
+];
